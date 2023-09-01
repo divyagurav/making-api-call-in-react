ebe09e87c7639e9d1940f258a118142d886d4bbf
divyagurav <132669878+divyagurav@users.noreply.github.com>
2023-09-01 15:23:04 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+
+function App() {
+  const [movie, setMovie] = useState([]);
+  function fetchMoviesHandler() {
+    fetch("https://swapi.dev/api/films")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const transformedMovies = data.results.map((movieData) => {
+          return {
+            id: movie.episode_id,
+            title: movieData.title,
+            openingText: movieData.opening_crawl,
+            releaseDate: movieData.release_date,
+          };
+        });
+        setMovie(transformedMovies);
+      });
+  }
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={movie} />
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
